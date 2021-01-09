f46d479674d55a25dbc4bff98f972df84bd8d57f
starrier <starrier@starrier.org>
2021-01-09 15:01:29 +0800

    init

---
+++ src/test/java/org/starrier/imperatororder/ImperatorOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.starrier.imperatororder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperatorOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
